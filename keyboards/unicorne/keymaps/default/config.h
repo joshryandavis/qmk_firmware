f2167dd535a4f5bf741ae22d3b0955f29eac4b01
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2024-05-22 23:54:54 +0900

    updated keymaps for unikey

--- keyboards/unicorne/keymaps/default/config.h
+++ keyboards/unicorne/keymaps/default/config.h
@@ -37,16 +37,7 @@
 
 #if defined(RGB_MATRIX_ENABLE)
 #    undef RGB_DISABLE_WHEN_USB_SUSPENDED       // turn off effects when suspended
-#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
         // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-//#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
-#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS
-#   define RGB_MATRIX_HUE_STEP 32
-#   define RGB_MATRIX_SAT_STEP 64
-#   define RGB_MATRIX_VAL_STEP 64
-#   define RGB_MATRIX_SPD_STEP 20
-//#   define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-#   define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_ALPHAS_MODS
 
     //  Disable the animations you don't want/need.  You will need to disable a good number of these
     //  because they take up a lot of space.  Disable until you can successfully compile your firmware.
@@ -63,7 +54,7 @@
 #    undef ENABLE_RGB_MATRIX_CYCLE_ALL 	
 #    undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT 	
 #    undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN 	
-#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON 	
+#    undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON 	
 #    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN 	
 #    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL 	
 #    undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL 	
@@ -73,18 +64,18 @@
 #    undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS 	
 #    undef ENABLE_RGB_MATRIX_RAINDROPS 	
 #    undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS 	
-#   define ENABLE_RGB_MATRIX_HUE_BREATHING 	
+#    undef ENABLE_RGB_MATRIX_HUE_BREATHING 	
 #    undef ENABLE_RGB_MATRIX_HUE_PENDULUM 	
 #    undef ENABLE_RGB_MATRIX_HUE_WAVE 	
 #    undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL 	
-#   define ENABLE_RGB_MATRIX_PIXEL_FLOW 	
+#    undef ENABLE_RGB_MATRIX_PIXEL_FLOW 	
 #    undef ENABLE_RGB_MATRIX_PIXEL_RAIN 	
 
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    undef RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #   if !defined(OLED_ENABLE)
 #       define ENABLE_RGB_MATRIX_TYPING_HEATMAP 	
 #   endif
-#   define ENABLE_RGB_MATRIX_DIGITAL_RAIN 	
+#    undef ENABLE_RGB_MATRIX_DIGITAL_RAIN 	
 
     //  These modes introduce additional logic that can increase firmware size.
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
@@ -97,7 +88,7 @@
 #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS 	
 #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS 	
 #    undef ENABLE_RGB_MATRIX_SPLASH 	
-#   define ENABLE_RGB_MATRIX_MULTISPLASH 	
+#    undef  ENABLE_RGB_MATRIX_MULTISPLASH 	
 #    undef ENABLE_RGB_MATRIX_SOLID_SPLASH 	
 #    undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH 	
 #endif
